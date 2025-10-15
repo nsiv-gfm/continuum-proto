5adca191c83fee1997bb9db03ffa7b5ac3953e01
nsiv-gfm <nathaniel.smith@intervarsity.org>
2025-10-15 14:46:14 -0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
-<React code from the current document would go here>
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './gfm_cohort_prototype_smorgasbord_storyboard_app.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
